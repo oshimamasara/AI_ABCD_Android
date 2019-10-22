63b40b3d6125b5332944d3e9c3ea3a3f9dbcbd88
YOUR NAME <YOUR@EMAIL.com>
2019-10-22 11:39:48 +0900

    Initial commit

---
+++ app/src/main/java/com/oshimamasara/ai_abcd/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oshimamasara.ai_abcd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
